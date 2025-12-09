90bbe43daff6d77c3e6ecb71030075ccf389aec7
Sharjeel Baig <dr.sharjeel.6@gmail.com>
2025-12-09 19:22:54 +0500

    feat(search): enhance search function to include answer and images in response

--- src/functions/search.ts
+++ src/functions/search.ts
@@ -1,8 +1,11 @@
 import tvly from "../utils/tavily.ts";
 
 export async function search(query: string): Promise<any> {
-  const response = await tvly.search(query);
+  const response = await tvly.search(query, {includeAnswer:true, includeImages: true});
 
-  return {links: response?.results?.map((item: any) => (item?.url
-  ))}
+  return {
+    links: response?.results?.map((item: any) => (item?.url)),
+    answer:response?.answer,
+    images:response?.images,
+  }
 }
